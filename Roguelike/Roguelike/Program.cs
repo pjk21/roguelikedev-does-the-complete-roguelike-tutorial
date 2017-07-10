41732df7d9bc44104c0367cb9d7d5218dffad658
Patrick Kirk <pat.kirk@gmail.com>
2017-07-09 22:22:00 -0400

    BearLibTerminal skeleton project.

---
+++ Roguelike/Roguelike/Program.cs
@@ -0,0 +1,58 @@
+﻿using BearLib;
+using System.Drawing;
+
+namespace Roguelike
+{
+    class Program
+    {
+        public const int ScreenWidth = 80;
+        public const int ScreenHeight = 50;
+
+        static void Main(string[] args)
+        {
+            Initialize();
+
+            while (true)
+            {
+                Draw();
+
+                if (!Update())
+                {
+                    break;
+                }
+            }
+
+            Terminal.Close();
+        }
+
+        private static void Initialize()
+        {
+            Terminal.Open();
+            Terminal.Set($"window: size={ScreenWidth}x{ScreenHeight};");
+            Terminal.Set($"font: Cheepicus_8x8x2.png, size=16x16, codepage=437;");
+        }
+
+        private static bool Update()
+        {
+            var input = Terminal.Read();
+
+            switch (input)
+            {
+                case Terminal.TK_ESCAPE:
+                case Terminal.TK_CLOSE:
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static void Draw()
+        {
+            Terminal.Clear();
+
+            Terminal.Print(1, 1, "Hello World");
+
+            Terminal.Refresh();
+        }
+    }
+}
