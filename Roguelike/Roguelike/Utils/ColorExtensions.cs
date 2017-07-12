61c0b17abdde24c7194ef6a88b3ecd6949608242
Patrick Kirk <pat.kirk@gmail.com>
2017-07-11 22:26:52 -0400

    Add extension method for lerping colours.

---
+++ Roguelike/Roguelike/Utils/ColorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace Roguelike
+{
+    public static class ColorExtensions
+    {
+        public static Color Lerp(this Color color, Color other, float t = 0.5f)
+        {
+            var r = (int)(color.R + (other.R - color.R) * t);
+            var g = (int)(color.G + (other.G - color.G) * t);
+            var b = (int)(color.B + (other.B - color.B) * t);
+
+            return Color.FromArgb(r, g, b);
+        }
+    }
+}
