30c460a356820c9fbcb9f4f0b545876c724612d3
Patrick Kirk <pat.kirk@gmail.com>
2017-07-15 15:17:23 -0400

    Add smooth walls when rendering with sprites.

--- Roguelike/Roguelike/Render/SpriteRenderer.cs
+++ Roguelike/Roguelike/Render/SpriteRenderer.cs
@@ -40,8 +40,10 @@ namespace Roguelike.Render
                 }
                 else
                 {
+                    int offset = GetWallSpriteOffset(map, x, y);
+
                     Terminal.Color(Color.White);
-                    Terminal.Put(x - camera.X, y - camera.Y, TileSprites.Wall);
+                    Terminal.Put(x - camera.X, y - camera.Y, TileSprites.Wall + offset);
                 }
 
                 map.SetCellProperties(x, y, map.IsTransparent(x, y), map.IsWalkable(x, y), true);
@@ -57,11 +59,40 @@ namespace Roguelike.Render
                     }
                     else
                     {
+                        int offset = GetWallSpriteOffset(map, x, y);
+
                         Terminal.Color(Color.Gray);
-                        Terminal.Put(x - camera.X, y - camera.Y, TileSprites.Wall);
+                        Terminal.Put(x - camera.X, y - camera.Y, TileSprites.Wall + offset);
                     }
                 }
             }
+        }
+
+        private int GetWallSpriteOffset(Map map, int x, int y)
+        {
+            int index = 0;
+
+            if (y > 0 && !map.IsWalkable(x, y - 1))
+            {
+                index += 1;
+            }
+
+            if (x < map.Width - 1 && !map.IsWalkable(x + 1, y))
+            {
+                index += 2;
+            }
+
+            if (y < map.Height - 1 && !map.IsWalkable(x, y + 1))
+            {
+                index += 4;
+            }
+
+            if (x > 0 && !map.IsWalkable(x - 1, y))
+            {
+                index += 8;
+            }
+
+            return index;
         }
     }
 }
--- Roguelike/Roguelike/TileSprites.cs
+++ Roguelike/Roguelike/TileSprites.cs
@@ -3,6 +3,6 @@
     public static class TileSprites
     {
         public const int Wall = 0xE000;
-        public const int Floor = 0xE001;
+        public const int Floor = 0xE010;
     }
 }
